a4158a1e17eb24b4b722a9d5591305f9b8bd0ac9
CharlyCS <ccoronels@uni.pe>
2023-11-15 17:33:25 -0500

    Responsive design to skills and projects main

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -73,8 +73,10 @@ function Projects() {
         <h1 className="py-6 text-6xl font-ibmplex text-center text-white uppercase tl:text-5xl">Projects</h1>
         <div className="grid grid-cols-1 p-10 justify-center items-center gap-8">
       {/*  {projects.map((item, i) => <ProjectCard key={i} item={item} />)}*/} 
-          <div className="grid grid-cols-2 p-10 justify-center items-center gap-8 lg:grid-cols-1">
-            <div classname = "w-1/2">
+
+
+          <div className="grid grid-cols-2 p-10 justify-center items-center gap-8 tablet:grid tablet:grid-cols-1">
+            <div className = "w-full pl-16 order-1">
               <div className="img-box w-[450px] lg2:w-auto mx-auto flex justify-center items-center relative overflow-hidden rounded-3xl">
                   <img src={gateway} alt="" className='w-full' />
                   <div className="p absolute top-[-100%] left-0 h-full w-full text-center p-4 pt-20 bg-gradient-to-b from-blue-600 to-purple-500">
@@ -83,7 +85,7 @@ function Projects() {
                   </div>
               </div>
             </div>
-            <div classname = "w-full pl-16">
+            <div className = "w-full pl-16 order-2">
               <p className="text-center text-white-500">{"By employing advanced technology like GPS and data analytics, this system is poised to make significant contributions to wildlife conservation and scientific research. Develop a cutting-edge solution to monitor and track the movement and behavior of wildlife, aiding in conservation efforts and research."}</p>
               <div className="py-8 flex items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
               <HashLink smooth to="/gateway#gateway" className="text-white border-[1px] border-white border-solid py-1 px-8 hover:bg-white hover:text-black font-bold text-2xl">
@@ -93,17 +95,8 @@ function Projects() {
             </div>
           </div>
 
-          <div className="grid grid-cols-2 p-10 justify-center items-center gap-8 lg:grid-cols-1">
-            <div classname = "w-1/2">
-              <p className="text-center text-white-500">{"Our project focuses on the creation of an efficient air quality monitoring system utilizing Zigbee technology. Equipped with a network of sensors, this system can measure various air pollutants and environmental parameters in real time. The wireless Zigbee connectivity offers a cost-effective and scalable solution for managing air quality effectively."}</p>
-              <div className="py-8 flex items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
-              <HashLink smooth to="/zigbee#zigbee" className="text-white border-[1px] border-white border-solid py-1 px-8 hover:bg-white hover:text-black font-bold text-2xl">
-                More
-              </HashLink>
-              </div>
-            </div>
-            
-            <div classname = "w-full pl-16">
+          <div className="grid grid-cols-2 p-10 justify-center items-center gap-8 tablet:grid tablet:grid-cols-1">
+            <div className = "w-full pl-16 order-4 tablet:order-3">
               <div className="img-box w-[450px] lg2:w-auto mx-auto flex justify-center items-center relative overflow-hidden rounded-3xl">
                   <img src={zigbee} alt="" className='w-full' />
                   <div className="p absolute top-[-100%] left-0 h-full w-full text-center p-4 pt-20 bg-gradient-to-b from-blue-600 to-purple-500">
@@ -112,11 +105,18 @@ function Projects() {
                   </div>
               </div>
             </div>
+            <div className = "w-full pl-16 order-3 tablet:order-4">
+              <p className="text-center text-white-500">{"Our project focuses on the creation of an efficient air quality monitoring system utilizing Zigbee technology. Equipped with a network of sensors, this system can measure various air pollutants and environmental parameters in real time. The wireless Zigbee connectivity offers a cost-effective and scalable solution for managing air quality effectively."}</p>
+              <div className="py-8 flex items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+              <HashLink smooth to="/zigbee#zigbee" className="text-white border-[1px] border-white border-solid py-1 px-8 hover:bg-white hover:text-black font-bold text-2xl">
+                More
+              </HashLink>
+              </div>
+            </div>
           </div>
 
-
-          <div className="grid grid-cols-2 p-10 justify-center items-center gap-8 lg:grid-cols-1">
-            <div classname = "w-1/2">
+          <div className="grid grid-cols-2 p-10 justify-center items-center gap-8 tablet:grid tablet:grid-cols-1">
+            <div className = "w-full pl-16 order-5">
               <div className="img-box w-[450px] lg2:w-auto mx-auto flex justify-center items-center relative overflow-hidden rounded-3xl">
                   <img src={gpstracker} alt="" className='w-full' />
                   <div className="p absolute top-[-100%] left-0 h-full w-full text-center p-4 pt-20 bg-gradient-to-b from-blue-600 to-purple-500">
@@ -125,7 +125,7 @@ function Projects() {
                   </div>
               </div>
             </div>
-            <div classname = "w-full pl-16">
+            <div className = "w-full pl-16 order-6">
               <p className="text-center text-white-500">{"We're designing a versatile GPS tracker with dual communication capabilities, including 4G LTE and WiFi. This tracker provides real-time location tracking and two-way communication, making it ideal for various applications such as asset tracking, vehicle monitoring, and personal security."}</p>
               <div className="py-8 flex items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
               <HashLink smooth to="/gpstracker#gpstracker" className="text-white border-[1px] border-white border-solid py-1 px-8 hover:bg-white hover:text-black font-bold text-2xl">
@@ -136,16 +136,8 @@ function Projects() {
           </div>
 
 
-          <div className="grid grid-cols-2 p-10 justify-center items-center gap-8 lg:grid-cols-1">
-            <div classname = "w-1/2">
-              <p className="text-center text-white-500">{"Our project centers around the development of an ESP32-based server capable of efficiently handling serial port requests. This server acts as a bridge between devices and applications that require serial communication, facilitating seamless data exchange and control in IoT and automation scenarios."}</p>
-              <div className="py-8 flex items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
-              <HashLink smooth to="/espserver#espserver" className="text-white border-[1px] border-white border-solid py-1 px-8 hover:bg-white hover:text-black font-bold text-2xl">
-                More
-              </HashLink>
-              </div>
-            </div>
-            <div classname = "w-full pl-16">
+          <div className="grid grid-cols-2 p-10 justify-center items-center gap-8 tablet:grid tablet:grid-cols-1">
+            <div className = "w-full pl-16 order-8 tablet:order-7">
               <div className="img-box w-[450px] lg2:w-auto mx-auto flex justify-center items-center relative overflow-hidden rounded-3xl">
                   <img src={espserver} alt="" className='w-full' />
                   <div className="p absolute top-[-100%] left-0 h-full w-full text-center p-4 pt-20 bg-gradient-to-b from-blue-600 to-purple-500">
@@ -154,11 +146,19 @@ function Projects() {
                   </div>
               </div>            
             </div>
+            <div className = "w-full pl-16 order-7 tablet:order-8">
+              <p className="text-center text-white-500">{"Our project centers around the development of an ESP32-based server capable of efficiently handling serial port requests. This server acts as a bridge between devices and applications that require serial communication, facilitating seamless data exchange and control in IoT and automation scenarios."}</p>
+              <div className="py-8 flex items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+              <HashLink smooth to="/espserver#espserver" className="text-white border-[1px] border-white border-solid py-1 px-8 hover:bg-white hover:text-black font-bold text-2xl">
+                More
+              </HashLink>
+              </div>
+            </div>
           </div>
 
 
-          <div className="grid grid-cols-2 p-10 justify-center items-center gap-8 lg:grid-cols-1">
-            <div classname = "w-1/2">
+          <div className="grid grid-cols-2 p-10 justify-center items-center gap-8 tablet:grid tablet:grid-cols-1">
+            <div className = "w-full pl-16 order-9">
               <div className="img-box w-[450px] lg2:w-auto mx-auto flex justify-center items-center relative overflow-hidden rounded-3xl">
                   <img src={jetson} alt="" className='w-full' />
                   <div className="p absolute top-[-100%] left-0 h-full w-full text-center p-4 pt-20 bg-gradient-to-b from-blue-600 to-purple-500">
@@ -167,7 +167,7 @@ function Projects() {
                   </div>
               </div>
             </div>
-            <div classname = "w-full pl-16">
+            <div className = "w-full pl-16 order-10">
               <p className="text-center text-white-500">{"We're creating a customized expansion board tailored to enhance the capabilities of the Jetson Nano platform for AI and machine learning projects. This specialized board offers additional interfaces, connectors, and features to enable seamless integration of various sensors and peripherals."}</p>
               <div className="py-8 flex items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
               <HashLink smooth to="/jetson#jetson" className="text-white border-[1px] border-white border-solid py-1 px-8 hover:bg-white hover:text-black font-bold text-2xl">
@@ -177,16 +177,8 @@ function Projects() {
             </div>
           </div>
 
-          <div className="grid grid-cols-2 p-10 justify-center items-center gap-8 lg:grid-cols-1">
-            <div classname = "w-1/2">
-              <p className="text-center text-white-500">{"Empower your hardware projects with our PIC18F4550 Development Board. Our experts have optimized it for maximum functionality and ease of use. With this board in your toolkit, your embedded systems development just got a whole lot smarter and efficient."}</p>
-              <div className="py-8 flex items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
-              <HashLink smooth to="/pic18f4550#pic18f4550" className="text-white border-[1px] border-white border-solid py-1 px-8 hover:bg-white hover:text-black font-bold text-2xl">
-                More
-              </HashLink>
-              </div>
-            </div>
-            <div classname = "w-full pl-16">
+          <div className="grid grid-cols-2 p-10 justify-center items-center gap-8 tablet:grid tablet:grid-cols-1">
+            <div className = "w-full pl-16 order-12 tablet:order-11">
               <div className="img-box w-[450px] lg2:w-auto mx-auto flex justify-center items-center relative overflow-hidden rounded-3xl">
                   <img src={pic18f4550} alt="" className='w-full' />
                   <div className="p absolute top-[-100%] left-0 h-full w-full text-center p-4 pt-20 bg-gradient-to-b from-blue-600 to-purple-500">
@@ -195,10 +187,18 @@ function Projects() {
                   </div>
               </div>            
             </div>
+            <div className = "w-full pl-16 order-11 tablet:order-12">
+              <p className="text-center text-white-500">{"Empower your hardware projects with our PIC18F4550 Development Board. Our experts have optimized it for maximum functionality and ease of use. With this board in your toolkit, your embedded systems development just got a whole lot smarter and efficient."}</p>
+              <div className="py-8 flex items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+              <HashLink smooth to="/pic18f4550#pic18f4550" className="text-white border-[1px] border-white border-solid py-1 px-8 hover:bg-white hover:text-black font-bold text-2xl">
+                More
+              </HashLink>
+              </div>
+            </div>
           </div>
 
-          <div className="grid grid-cols-2 p-10 justify-center items-center gap-8 lg:grid-cols-1">
-            <div classname = "w-1/2">
+          <div className="grid grid-cols-2 p-10 justify-center items-center gap-8 tablet:grid tablet:grid-cols-1">
+            <div className = "w-full pl-16 order-13">
               <div className="img-box w-[450px] lg2:w-auto mx-auto flex justify-center items-center relative overflow-hidden rounded-3xl">
                   <img src={mpost} alt="" className='w-full' />
                   <div className="p absolute top-[-100%] left-0 h-full w-full text-center p-4 pt-20 bg-gradient-to-b from-blue-600 to-purple-500">
@@ -207,7 +207,7 @@ function Projects() {
                   </div>
               </div>
             </div>
-            <div classname = "w-full pl-16">
+            <div className = "w-full pl-16 order-14">
               <p className="text-center text-white-500">{"Unlock the potential of your MEI Cashflow Bill Acceptors with our MPOST C++ Library. Our expertise in firmware development ensures seamless integration and advanced features. Say goodbye to compatibility issues and hello to enhanced functionality."}</p>
               <div className="py-8 flex items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
               <HashLink smooth to="/mpost#mpost" className="text-white border-[1px] border-white border-solid py-1 px-8 hover:bg-white hover:text-black font-bold text-2xl">
